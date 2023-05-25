d78f95ba8bbe8aea14e6bd0c2d165e77dd7cac69
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-05-25 19:45:51 +0200

    added first migration files

---
+++ SmartCafe/SmartCafe/Models/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheSmartCafe.Model
+{
+    public class Owner
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public Owner() { }
+    }
+}
