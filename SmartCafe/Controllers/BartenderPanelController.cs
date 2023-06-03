149f8e88309219c12b74a6267c218819dbf5d6be
eefendic1 <115942770+eefendic1@users.noreply.github.com>
2023-06-03 23:57:47 +0200

    Updates in bartender panel

--- SmartCafe/Controllers/BartenderPanelController.cs
+++ SmartCafe/Controllers/BartenderPanelController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SmartCafe.Data;
+using SmartCafe.Models;
 using System;
+using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SmartCafe.Controllers
 {
+    [Authorize(Roles = "Konobar")]
     public class BartenderPanelController : Controller
     {
 
@@ -56,47 +63,94 @@ namespace SmartCafe.Controllers
             }
         }
 
-        // GET: BartenderPanelController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["idGuest"] = new SelectList(_context.Guests, "id", "id", order.idGuest);
+            return View(order);
         }
 
-        // POST: BartenderPanelController/Edit/5
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, [Bind("id,done,tableNumber,orderTime,idGuest")] Order order)
         {
-            try
+            if (id != order.id)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            ViewData["idGuest"] = new SelectList(_context.Guests, "id", "id", order.idGuest);
+            return View(order);
         }
 
-        // GET: BartenderPanelController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Guest)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
         }
 
-        // POST: BartenderPanelController/Delete/5
-        [HttpPost]
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var order = await _context.Orders.FindAsync(id);
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.id == id);
+        }
+    }
 
     }
-}
+
