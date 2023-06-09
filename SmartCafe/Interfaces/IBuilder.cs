bd15152fe28fcf6445e9c0ffc131f473b67d95f0
eefendic1 <115942770+eefendic1@users.noreply.github.com>
2023-06-09 20:49:32 +0200

    Update in patterns

---
+++ SmartCafe/Interfaces/IBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace SmartCafe.Interfaces
+{
+    public interface IBuilder
+    {
+        public void setOrderNumber(int  orderNumber);
+        public void setOrderState(bool state);
+        public void setTableNumber(int tableNumber);
+        public void setOrderTime(System.DateTime dateTime);
+    }
+}
---
+++ SmartCafe/Interfaces/IState.cs
@@ -0,0 +1,9 @@
+﻿using SmartCafe.Models;
+
+namespace SmartCafe.Interfaces
+{
+    public interface IState
+    {
+        public void ShowOrderDetails(Order order);
+    }
+}
